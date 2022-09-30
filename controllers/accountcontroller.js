cc51c36adf727516487f5a6ebfd96059fdfb1099
atish singh <atishsingh226@gmail.com>
2022-09-30 22:59:59 +0530

    Make changes

--- controllers/accountcontroller.js
+++ controllers/accountcontroller.js
@@ -25,7 +25,7 @@ exports.remove=async (req, res)=>{
         res.send(result);
     };
     
-exports.update=async (req, res)=>{
+    exports.update=async (req, res)=>{
         let result=[];
         result=await dal.update(req.params.id)
         res.send(result);
