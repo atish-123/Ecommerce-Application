6d871db72f3e7829704cc07a67772cdcfefdf266
atish singh <atishsingh226@gmail.com>
2022-10-02 22:33:28 +0530

    create a JWT

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express=require('express');
+const express=require('express-session');
 const routes=require('./router');
 //var path=require('path');
 
-var app=express();
+const oneDay=1000 * 60 * 60 * 24;
 
+
+const app=express();
 
 app.use(express.urlencoded({extended:true}));
 
 app.use(express.json());
+
+app.set("view engine","ejs");
+
+var sessionMiddleware = expressionSession({
+    secret:"product",
+    saveUninitalized: true,
+    cookie:{maxage: oneDay},
+    resave:false
+
+});
+app.use(sessionMiddleware);
 
 
 routes(app);
