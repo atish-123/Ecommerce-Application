547971372fb9402142724819c83202bdc4c3ae4c
atish singh <atishsingh226@gmail.com>
2022-10-03 23:40:31 +0530

    create the login page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,14 +4,23 @@ var fetchData=()=>{
     let url="http//localhost:3000/api/products";
 
     $.ajax({
-        datatype:"json",
-        url:url,
-        success:(data)=>{
-            console.log("data");
-            let strData
-            }
-
-
+        dataType: "json",
+        url: url,
+        success: (data)=>{
+            console.log(data);
+            let strData=JSON.stringify(data)
+            //alert(strData);
+           /* let para1=document.getElementById("para");
+            para1.innerHTML=strData;
+            */
+            //DOM tree Manipulation Code at Client Side
+            let productList=document.getElementById("productList");
+            for(var i=0;i<data.length;i++){
+                const node = document.createElement("li");
+                const textnode = document.createTextNode(data[i].title);
+                node.appendChild(textnode);
+                productList.appendChild(node);
+            }  
         }
-    })
-}
+      });
+    }
