92a73c5f964487499a825d32c2904c9fa359f985
atish singh <atishsingh226@gmail.com>
2022-10-04 01:22:28 +0530

    Changes in commit

---
+++ Backend/controllers/customercontroller.js
@@ -0,0 +1,32 @@
+const { response } = require('express');
+
+const dal=require('../models/customers')
+
+
+
+exports.customerPage = async(req,res)=>{
+  res.send()
+
+exports.getById= async function(req, res){  
+  let result=[];
+  result=await dal.getById(req.params.id);
+  res.send(result); 
+};
+
+exports.insert=async(req, res)=>{
+  let result=[];
+  result=await dal.insert(req);
+  res.send(result);
+  };
+
+exports.remove=async (req, res)=>{
+      let result=[];
+      result=await dal.remove(req.params.id)
+      res.send(result);
+};
+  
+
+
+    
+
+
