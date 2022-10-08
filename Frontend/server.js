ce0798fa942ebaaf956993a10b995ea1280f57df
atish singh <atishsingh226@gmail.com>
2022-10-08 13:32:13 +0530

    changin in frontend

--- Frontend/server.js
+++ Frontend/server.js
@@ -4,7 +4,7 @@ app.use(express.static('public'));
 app.get("/",(req,res)=>{
     res.sendFile("index.html");
 })
-
+app.use(express);
 app.listen(7000);
 console.log("server Side Javascript Code");
 console.log("Website is hostyed on webserver.listening on port 7000");
